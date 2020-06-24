40c625770ed8a6595ea9a331994b6573492967a2
Devansh Sharma <devansharma92@gmail.com>
2020-06-24 16:44:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,22 +5,26 @@ Created on Mon Jun 22 00:11:38 2020
 @author: Mahesh Sharma
 """
 from flask import Flask,render_template,url_for,request
-import pandas as pd
+import pandas as pd 
+
 import pickle
 
+# load the model from disk
 loaded_model=pickle.load(open('xgboost_model.pkl', 'rb'))
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     df=pd.read_csv('real_2018.csv')
     my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
-    my_prediction = my_prediction.tolist()
-    return render_template('result.html',prediction=my_prediction)
+    my_prediction=my_prediction.tolist()
+    return render_template('result.html',prediction = my_prediction)
 
-if __name__=='__main__':
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
